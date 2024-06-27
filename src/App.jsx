c5fe188665738f2e77faf46eba727cfc657007e5
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-06-27 18:07:04 -0500

    💄 feat: Improve error handling in search form input styling

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,17 @@ function App() {
       <header className='header'>
         <h1>Movies search</h1>
         <form className='form' onSubmit={handlerSubmit}>
-          <input name='query' type="text" value={search} placeholder='Avengers, Matrix, Cars...' onInput={handlerInput}/>
+          <input
+            name='query'
+            type="text"
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }}
+            value={search}
+            placeholder='Avengers, Matrix, Cars...'
+            onInput={handlerInput}
+          />
           <button type='submit'>Search</button>
         </form>
         {error && <p style={{color:'red'}}>{error}</p>}
