82eb2641b4577e385d86e52a28ccb9fbcb39ea11
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-06-27 16:24:43 -0500

    ✨ feat: add useSearch hook to manage search and errors

---
+++ src/hooks/useSearch.js
@@ -0,0 +1,29 @@
+import { useEffect, useRef, useState } from 'react'
+
+export function useSearch() {
+    const [search, setSearch] = useState('')
+    const [error, setError] = useState(null)
+    const isFirstSearch = useRef(true)
+
+    useEffect( () => {
+        if (isFirstSearch.current) {
+            isFirstSearch.current = search === ''
+            return
+        }
+        if (search === '') {
+            setError('No se puede hacer una busqueda vacia')
+            return
+        }
+        if(search.length < 3) {
+            setError('Debe contener al menos 3 caracteres las busqueda');
+            return
+        }
+        if (search.match(/\d/)) {
+            setError('No se puede buscar una pelicula con un numero')
+            return
+        }
+        setError(null)
+    }, [search])
+
+    return { search, error, setSearch}
+}
