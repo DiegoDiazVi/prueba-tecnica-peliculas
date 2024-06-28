e08bfa2087777eb148220cfad80d843dc815147b
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-06-27 19:05:33 -0500

    ✨ feat: Add useCallback to getMovies function in useMovies hook

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useCallback } from 'react'
 import { fetchMovies } from '../services/fetchMovies';
 
 function useMovies({search}) {
@@ -6,20 +6,20 @@ function useMovies({search}) {
     const [movies, setMovies] = useState([])
     const [loading, setLoading] = useState(false)
 
-    const getMovies = async () => {
-        if((search === '') || (previousSearch.current === search)) return
+    const getMovies =  useCallback(async ({ search }) => {
+            if((search === '') || (previousSearch.current === search)) return
 
-        try {
-            setLoading(true)
-            const response = await fetchMovies(search)
-            setMovies(response)
-            previousSearch.current = search
-        } catch (error) {
-            console.error(error.message)
-        } finally {
-            setLoading(false)
-        }
-    }
+            try {
+                setLoading(true)
+                const response = await fetchMovies(search)
+                setMovies(response)
+                previousSearch.current = search
+            } catch (error) {
+                console.error(error.message)
+            } finally {
+                setLoading(false)
+            }
+        }, [])
 
     return { movies, getMovies, loading }
 }
